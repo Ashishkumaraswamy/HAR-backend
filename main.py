facc8228ec7e922f0bd4763c43c22306ee7612b3
Ashish <ashishkumaraswamy@gmail.com>
2021-10-20 07:39:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,10 @@ def get_data_from_app():
     #gyroscope=request.json['gyroscope']
     #accelerometer=request.json['accelerometer']
     # hello=request.json['hello']
+    gyroscope=np.array(input_json['gyroscope'])*10
+    accelerometer=np.array(input_json['accelerometer'])*10
     shape = list(np.array(input_json['gyroscope']).shape)
-    data= fe.extract_features(np.array(input_json['gyroscope'])*10,np.array(input_json['accelerometer'])*10)
+    data= fe.extract_features(gyroscope,accelerometer)
     f_2 = data
     #loaded_model = pickle.load(open('knnpickle_file', 'rb'))
     loaded_model = pickle.load(open('lrmodel(2).pkl', 'rb'))
@@ -24,8 +26,8 @@ def get_data_from_app():
     data = np.array(data)
     pred = outputlabel[int(loaded_model.predict(data.reshape(1,81)))]
     dictToReturn = {'data' : f_2 ,'output': pred,'shape':shape}
-    return jsonify(dictToReturn)
-    #return jsonify(gyroscope=gyroscope,accelerometer=accelerometer)
+    # return jsonify(dictToReturn)
+    return jsonify(gyroscope=gyroscope,accelerometer=accelerometer)
 
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=4000,debug=True)
