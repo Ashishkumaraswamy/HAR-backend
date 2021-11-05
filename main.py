ab65ba60027331d7d7932454105bbb8060e9c24b
Ashish <ashishkumaraswamy@gmail.com>
2021-11-05 20:12:16 +0530

    changes made

--- main.py
+++ main.py
@@ -13,39 +13,13 @@ def home():
     return '<html><body>hello world</body></html>'
 
 
-# @app.route('/send',methods=['POST'])
-# def get_data_from_app():
-#     output_list = ['Biking','Downstairs','joging','siting','standing','upstairs','walking']
-   
-#     N_TIME_STEPS = 100 #sliding window length
-#     STEP = 50 #Sliding window step size
-#     N_FEATURES = 12 
-#     input_json = request.get_json(force=True)
-#     gyroscope=input_json['gyroscope']
-#     accelerometer=input_json['accelerometer']
-#     accelerometer_gravity=input_json['accelerometer_gravity']
-#     gyroscope=np.array(gyroscope)
-#     accelerometer=np.array(accelerometer)
-#     accelerometer_gravity=np.array(accelerometer_gravity)
-#     gyroscope.reshape(100,3)
-#     accelerometer.reshape(100,3)
-#     accelerometer_gravity.reshape(100,3)
-#     body = np.subtract(accelerometer,accelerometer_gravity)
-#     temp = np.hstack((accelerometer_gravity,body,gyroscope))
-#     df = pd.DataFrame(temp ,columns=['Ax','Ay','Az','Lx','Ly','Lz','Gx','Gy','Gz'])
-#     data = df
-#     test_X=fe.concat(data)
-
-#     test_X=fe.generate_sequence(test_X,N_TIME_STEPS, STEP)
-#     X_test=fe.reshape_segments(test_X,N_TIME_STEPS, N_FEATURES)
-#     model= keras.models.load_model('keras_model.h5')
-#     pred= model.predict(X_test)
-#     result = output_list[np.argmax(pred)]
-#     dictToReturn = {'output': result}
-#     return jsonify(output=dictToReturn)
-
 @app.route('/send',methods=['POST'])
 def get_data_from_app():
+    output_list = ['Biking','Downstairs','joging','siting','standing','upstairs','walking']
+   
+    N_TIME_STEPS = 100 #sliding window length
+    STEP = 50 #Sliding window step size
+    N_FEATURES = 12 
     input_json = request.get_json(force=True)
     gyroscope=input_json['gyroscope']
     accelerometer=input_json['accelerometer']
@@ -53,25 +27,51 @@ def get_data_from_app():
     gyroscope=np.array(gyroscope)
     accelerometer=np.array(accelerometer)
     accelerometer_gravity=np.array(accelerometer_gravity)
-    gyroscope.reshape(128,3)
-    accelerometer.reshape(128,3)
-    accelerometer_gravity.reshape(128,3)
+    gyroscope.reshape(100,3)
+    accelerometer.reshape(100,3)
+    accelerometer_gravity.reshape(100,3)
     body = np.subtract(accelerometer,accelerometer_gravity)
-    scaler=MinMaxScaler(feature_range=(-1,1))
-    accelerometer_gravity=scaler.fit_transform(accelerometer_gravity)
-    body=scaler.fit_transform(body)
-    gyroscope=scaler.fit_transform(gyroscope)
-    shape = list(np.array(input_json['gyroscope']).shape)
-    data= fe.extract_features(gyroscope,body,accelerometer_gravity)
-    f_2 = data
-    #loaded_model = pickle.load(open('knnpickle_file', 'rb'))
-    loaded_model = pickle.load(open('rdclfmodel.pkl', 'rb'))
-    outputlabel=['LAYING','SITTING','STANDING','WALKING','WALKING_DOWNSTAIRS','WALKING_UPSTAIRS']
-    data = np.array(data)
-    # pred = outputlabel[int(loaded_model.predict(data.reshape(1,81)))]
-    pred=loaded_model.predict(data.reshape(1,81))
-    dictToReturn = {'data' : f_2 ,'output': pred[0],'shape':shape}
-    return jsonify(dictToReturn)
+    temp = np.hstack((accelerometer_gravity,body,gyroscope))
+    df = pd.DataFrame(temp ,columns=['Ax','Ay','Az','Lx','Ly','Lz','Gx','Gy','Gz'])
+    data = df
+    test_X=fe.concat(data)
+
+    test_X=fe.generate_sequence(test_X,N_TIME_STEPS, STEP)
+    X_test=fe.reshape_segments(test_X,N_TIME_STEPS, N_FEATURES)
+    model= keras.models.load_model('keras_model.h5')
+    pred= model.predict(X_test)
+    result = output_list[np.argmax(pred)]
+    dictToReturn = {'output': result}
+    return jsonify(output=dictToReturn)
+
+# @app.route('/send',methods=['POST'])
+# def get_data_from_app():
+#     input_json = request.get_json(force=True)
+#     gyroscope=input_json['gyroscope']
+#     accelerometer=input_json['accelerometer']
+#     accelerometer_gravity=input_json['accelerometer_gravity']
+#     gyroscope=np.array(gyroscope)
+#     accelerometer=np.array(accelerometer)
+#     accelerometer_gravity=np.array(accelerometer_gravity)
+#     gyroscope.reshape(128,3)
+#     accelerometer.reshape(128,3)
+#     accelerometer_gravity.reshape(128,3)
+#     body = np.subtract(accelerometer,accelerometer_gravity)
+#     scaler=MinMaxScaler(feature_range=(-1,1))
+#     accelerometer_gravity=scaler.fit_transform(accelerometer_gravity)
+#     body=scaler.fit_transform(body)
+#     gyroscope=scaler.fit_transform(gyroscope)
+#     shape = list(np.array(input_json['gyroscope']).shape)
+#     data= fe.extract_features(gyroscope,body,accelerometer_gravity)
+#     f_2 = data
+#     #loaded_model = pickle.load(open('knnpickle_file', 'rb'))
+#     loaded_model = pickle.load(open('rdclfmodel.pkl', 'rb'))
+#     outputlabel=['LAYING','SITTING','STANDING','WALKING','WALKING_DOWNSTAIRS','WALKING_UPSTAIRS']
+#     data = np.array(data)
+#     # pred = outputlabel[int(loaded_model.predict(data.reshape(1,81)))]
+#     pred=loaded_model.predict(data.reshape(1,81))
+#     dictToReturn = {'data' : f_2 ,'output': pred[0],'shape':shape}
+#     return jsonify(dictToReturn)
 
 
 
