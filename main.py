65171a90f7ee55b4a5232da978bb3b1978f4a781
Ashish <ashishkumaraswamy@gmail.com>
2021-11-18 20:15:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def send_prob_to_app():
 @app.route('/send',methods=['POST'])
 def get_data_from_app():
     
-    output_list = ['Biking' ,' Downstairs', 'Jogging', 'Sitting', 'Standing', 'Upstairs', 'Walking']
+    output_list = ['Biking' ,'Walking', 'Jogging', 'Sitting', 'Standing', 'Upstairs', 'Downstairs']
    
     N_TIME_STEPS = 100 #sliding window length
     STEP = 50 #Sliding window step size
